c8f092c0bafc4f0f79aa0d5a6f6c3aff50e864e0
teddingdev <teddingdev@gmail.com>
2020-05-12 03:27:27 +0800

    set a clock

--- src/app/pages/data-page/data-page.component.ts
+++ src/app/pages/data-page/data-page.component.ts
@@ -7,11 +7,9 @@ import { GithubService } from '../../services/github.service'
   styleUrls: ['./data-page.component.sass']
 })
 export class DataPageComponent implements OnInit {
+  currentTime = new Date().toISOString()
+
   data = [
-    {
-      propertyName: 'Current Date',
-      value: new Date().toISOString()
-    },
     {
       propertyName: 'Repository Name',
       value: undefined
@@ -33,7 +31,7 @@ export class DataPageComponent implements OnInit {
       value: undefined
     },
     {
-      propertyName: 'open_issues_count ',
+      propertyName: 'open_issues_count',
       value: undefined
     }
   ];
@@ -41,13 +39,12 @@ export class DataPageComponent implements OnInit {
   constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.currentTime = new Date().toISOString();
+    }, 1000);
     this.githubService.getRepo().subscribe(data => {
       console.log(data);
-      this.data.reduce((prev, curr) => {
-        return {
-          ...curr, value: data[curr.propertyName]
-        };
-      });
+      this.data = this.data.map((item) => ({ ...item, value: data[item.propertyName] ? data[item.propertyName] : item.value }));
     });
   }
 
